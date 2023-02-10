95e442b2452a776df8d32843b79202198f82c99a
13reath <new13reath@gmail.com>
2023-02-10 13:47:58 +0200

    v 1.0.1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'strict';
 
 // ELEMENTS
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal-box');
+const overlay = document.querySelector('.modal__overlay');
 
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.show-modal');
+const btnCloseModal = document.querySelector('.modal--close');
+const btnOpenModal = document.querySelector('.modal--show');
 
 let interval = 1000;
 
@@ -24,20 +24,12 @@ const openModal = function () {
     overlay.classList.remove('hidden');
 };
 
-// ---------- Open modal ----------
-
-// On click
 btnOpenModal.addEventListener('click', openModal);
 
-// ---------- Closing modal ----------
-
-// By pressing on X
 btnCloseModal.addEventListener('click', closeModal);
 
-// By pressing on overlay
 overlay.addEventListener('click', closeModal);
 
-// By pressing ESCAPE button
 document.addEventListener('keydown', function (button) {
     if (button.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
 });
@@ -51,16 +43,13 @@ const changeColor = (value, str) => (label.innerHTML = str.fontcolor(value));
 const changeText = (str) => (label.textContent = str);
 
 const labelLogic = function () {
-    // Generating a random number
     let string = Math.random().toString(36).slice(2, 7);
 
-    // If not #3 then continue
     if (!string.includes(0)) {
         changeText(string);
-        // Now check #2 and #1
-        // Checking if string consists of all digits
+
         if (/^\d+$/.test(string)) return changeColor('blue', string);
-        // Checking if it is palindome
+
         if (string.slice(0, 2) === string.slice(-2).split('').reverse().join('')) changeColor('red', string);
     } else changeText('There was zero');
 };
